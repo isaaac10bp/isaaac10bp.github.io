5dd28559989e04284309d83f6ab47607646699a7
Isaac Barrientos <isaac10bp@gmail.com>
2021-06-02 12:59:20 -0500

    v.1
    Made by: Isaac B, Marlon A. and Emmanuel R.

---
+++ js/formulario.js
@@ -0,0 +1,8 @@
+let menu = document.querySelector("#menu");
+let toggleIicon = document.querySelector("#toggle-icon");
+
+toggleIicon.addEventListener("click", function(){
+    menu.classList.toggle("menu-on");
+}
+);
+
